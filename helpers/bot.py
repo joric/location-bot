108b27f65e9352bbff436993479f91475321c33d
joric <joric@users.noreply.github.com>
2024-08-27 01:13:21 +0500

    updated files

--- helpers/bot.py
+++ helpers/bot.py
@@ -5,6 +5,10 @@ from telegram.ext import Dispatcher, MessageHandler, CommandHandler, Filters
 
 from uuid import uuid4
 
+import requests
+import urllib.parse
+
+
 logger = logging.getLogger(__name__)
 
 def start(update, context):
@@ -15,6 +19,14 @@ Example:
 """
     )
 
+def getLocationFromOSM(title):
+    q = urllib.parse.quote_plus(context.args[0])
+    url = f'https://nominatim.openstreetmap.org/search?q={q}&format=json&limit=1'
+    res = requests.get(url)
+    data = json.loads(res.text)
+    if data:
+        return map(float, [data[0]['lat'], data[0]['lon']])
+
 def echo(update, context):
     update.effective_message.reply_text(update.effective_message.text)
 
@@ -24,8 +36,12 @@ def location(update, context):
         longitude = float(context.args[1])
         update.effective_message.reply_location(latitude=latitude, longitude=longitude)
     except (IndexError, ValueError) as e:
-        # logger.error(f"Error processing location command: {e} - {context.args}")
-        update.effective_message.reply_text('Usage: /location <latitude> <longitude>')
+        try:
+            latitude, longitude = getLocationFromOSM(context.args[0])
+            update.effective_message.reply_location(latitude=latitude, longitude=longitude)
+        except (IndexError, ValueError) as e:
+            # logger.error(f"Error processing location command: {e} - {context.args}")
+            update.effective_message.reply_text('Usage: /location <latitude> <longitude>')
 
 def get_dispatcher(bot):
     """Create and return dispatcher instances"""
