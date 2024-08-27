4d2e8bb46bf261c9ce118ae1a991df5166402d0b
joric <joric@users.noreply.github.com>
2024-08-27 09:37:09 +0500

    updated files

--- helpers/bot.py
+++ helpers/bot.py
@@ -8,7 +8,7 @@ import json
 
 logger = logging.getLogger(__name__)
 
-def help(update, context):
+def start(update, context):
     """Send a message when the command /start is issued."""
     update.effective_message.reply_text("""Hi! I search location.
 Example: "40.714627 -74.002863" or "new york".
@@ -54,6 +54,7 @@ def get_dispatcher(bot):
     """Create and return dispatcher instances"""
     dispatcher = Dispatcher(bot, None, workers=0)
 
-    dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", start))
     dispatcher.add_handler(MessageHandler((Filters.text | Filters.update) & ~Filters.command, echo))
     return dispatcher
